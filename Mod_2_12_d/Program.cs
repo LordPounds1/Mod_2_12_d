a09f3fe395758590829621aa55e109e9a3631248
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 22:21:02 +0600

    Add project files.

---
+++ Mod_2_12_d/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("d. Введите значение a: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("d. Введите значение b (b > a): ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (b > a)
+        {
+            Console.WriteLine($"Все целые числа от {a} до {b}:");
+            for (int i = a; i <= b; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Значение b должно быть больше a.");
+        }
+    }
+}
